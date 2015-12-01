74147dfd49ab24f4a4dd22d218a70e80953a090e
Carlos Ruiz <carlos.ruiz.dominguez@west.cmu.edu>
2015-12-01 11:42:02 -0800

    HSV thresholding to improve line detection
    Use HSV thresholding instead of Canny edge detection to feed a B/W
    image to the Hough line detector

--- NeedleInStack/substation-findLine.py
+++ NeedleInStack/substation-findLine.py
@@ -2,32 +2,49 @@ __author__ = 'zhengyiwang'
 
 import cv2
 import numpy as np
+import datetime
+
+print "Start: " + str(datetime.datetime.now())
 
 img = cv2.imread("findLine-substation-3.png")
+img = cv2.imread("../training-images/substation_31/" + "substation_31_img1_y38.191553_x-120.82906_z20" + ".png")
 
-# cv2.imshow('test-hsv.jpg', np.hstack((cv2.Canny(equ, 500, 1000), cv2.Canny(equ, 300, 500), cv2.Canny(equ, 300, 1000), cv2.Canny(equ, 900, 1000))))
+#edges = cv2.Canny(img, 100, 500)
+img_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+#print "Mean HSV: " + str(cv2.mean(img_HSV))
+if cv2.mean(img_HSV)[2] < 100:
+    edges = cv2.inRange(img_HSV, np.array([0, 0, 120], np.uint8), np.array ([180, 100, 255], np.uint8))
+else:
+    edges = cv2.inRange(img_HSV, np.array([0, 0, 0], np.uint8), np.array ([180, 255, 100], np.uint8))
+    edges = cv2.inRange(img_HSV, np.array([0, 0, 160], np.uint8), np.array ([180, 25, 255], np.uint8))
 
-edges = cv2.Canny(img, 100, 500)
-lines = cv2.HoughLinesP(edges, 1, np.pi/180, 75, minLineLength=400, maxLineGap=30)
+print "Edges detected: " + str(datetime.datetime.now())
+
+lines = cv2.HoughLinesP(edges, 1, np.pi/180, 75, minLineLength=250, maxLineGap=30)
 # lines = cv2.HoughLines(edges, 1, np.pi/180, 175)
 
-for line in lines:
-    line = line.ravel()
-    cv2.line(img, tuple(line[0:2]), tuple(line[2:4]), (0, 0, 255), 2)
-    continue
-    for rho, theta in line:
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a*rho
-        y0 = b*rho
-        x1 = int(x0 + 1000*(-b))
-        y1 = int(y0 + 1000*a)
-        x2 = int(x0 - 1000*(-b))
-        y2 = int(y0 - 1000*a)
+print "Lines detected: " + str(datetime.datetime.now())
 
-        cv2.line(img, (x1,y1), (x2,y2), (0,0,255), 2)
+if lines is not None:
+    for line in lines:
+        line = line.ravel()
+        cv2.line(img, tuple(line[0:2]), tuple(line[2:4]), (0, 0, 255), 2)
+        continue
+        for rho, theta in line:
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a*rho
+            y0 = b*rho
+            x1 = int(x0 + 1000*(-b))
+            y1 = int(y0 + 1000*a)
+            x2 = int(x0 - 1000*(-b))
+            y2 = int(y0 - 1000*a)
+
+            cv2.line(img, (x1,y1), (x2,y2), (0,0,255), 2)
+
+print "End: " + str(datetime.datetime.now())
 
 cv2.imshow('Result', np.hstack((img, cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB))))
 cv2.imwrite('findLine-lines-3.jpg', img)
 cv2.imwrite('findLine-edges-3.jpg', edges)
-cv2.waitKey()
+cv2.waitKey(10000)
--- training-images/download_training_img_from_coords.py
+++ training-images/download_training_img_from_coords.py
@@ -17,16 +17,16 @@ def parse_coords_file(file_name):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('-file', type=str, default='coordinates-manual_WIP.txt',
+    parser.add_argument('-file', type=str, default='coordinates_test.txt',
                        help='path to file that contains substation coordinates')
-    parser.add_argument('-radius', type=int, default=5,
+    parser.add_argument('-radius', type=int, default=0,
                        help='distance (in image widths) around the substation center within which we want to get images')
     parser.add_argument('-zoom', type=int, default=20,
                        help='zoom level at which to get the satellite images')
     args = parser.parse_args()
 
     coords = parse_coords_file(args.file)
-    cnt_subs = 0
+    cnt_subs = 30
     for coord in coords:
         [y_ctr, x_ctr] = [float(a) for a in coord.split(',')]
         cnt_subs += 1
@@ -42,15 +42,15 @@ if __name__ == "__main__":
                 req = urllib2.Request("https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center=" +
                         str(y_curr) + "," + str(x_curr) + "&zoom=" + str(args.zoom) + "&size=640x640&key=" + GMAPS_KEY)
                 image = urllib2.urlopen(req).read()
-                out_file_name = "substation_" + str(cnt_subs) + "/" + "substation_" + str(cnt_subs) + "_img" + str(cnt_img) + "_y" + str(y_curr) + "_x" + str(x_curr) + "_z" + str(args.zoom) + ".png"
-
-                with open(out_file_name, 'wb') as f:
-                    f.write(image)
 
                 if x == 0 and y == 0:
                     out_file_name = "substation_" + str(cnt_subs) + "_y" + str(y_curr) + "_x" + str(x_curr) + "_z" + str(args.zoom) + ".png"
                     with open(out_file_name, 'wb') as f:
                         f.write(image)
+
+                out_file_name = "substation_" + str(cnt_subs) + "/" + "substation_" + str(cnt_subs) + "_img" + str(cnt_img) + "_y" + str(y_curr) + "_x" + str(x_curr) + "_z" + str(args.zoom) + ".png"
+                with open(out_file_name, 'wb') as f:
+                    f.write(image)
 
                 print "\tImage " + str(cnt_img) + "/" + str((2*args.radius + 1)**2) + " saved as '" + out_file_name + "'"
 
