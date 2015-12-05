f55ca77c5f868feb8703fddd46555d981712f6af
Carlos Ruiz <carlos.ruiz.dominguez@west.cmu.edu>
2015-12-04 17:17:27 -0800

    findLine algorithm improved

--- NeedleInStack/substation-findLine.py
+++ NeedleInStack/substation-findLine.py
@@ -3,21 +3,26 @@ __author__ = 'zhengyiwang'
 import cv2
 import numpy as np
 import datetime
+import math
+import matplotlib.pyplot as plt
+plt.ion()
 
 print "Start: " + str(datetime.datetime.now())
 
 img = cv2.imread("findLine-substation-3.png")
-img = cv2.imread("../training-images/substation_31/" + "substation_31_img1_y38.191553_x-120.82906_z20" + ".png")
+img = cv2.imread("../training-images/substation_31/" + "substation_3_img72_y44.7725058_x-122.6746753_z20" + ".png")
+img = cv2.imread("../training-pos/" + "substation_4_img71_y45.6526422_x-122.4998221_z20" + ".png")
 
 #edges = cv2.Canny(img, 100, 500)
 img_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 #print "Mean HSV: " + str(cv2.mean(img_HSV))
 if cv2.mean(img_HSV)[2] < 100:
-    edges = cv2.inRange(img_HSV, np.array([0, 0, 120], np.uint8), np.array ([180, 100, 255], np.uint8))
+    edges = cv2.inRange(img_HSV, np.array([0, 0, 120], np.uint8), np.array([180, 100, 255], np.uint8))
 else:
-    edges = cv2.inRange(img_HSV, np.array([0, 0, 0], np.uint8), np.array ([180, 255, 100], np.uint8))
-    edges = cv2.inRange(img_HSV, np.array([0, 0, 160], np.uint8), np.array ([180, 25, 255], np.uint8))
+    edges = cv2.inRange(img_HSV, np.array([0, 0, 0], np.uint8), np.array([180, 255, 100], np.uint8))
+    # edges = cv2.inRange(img_HSV, np.array([0, 0, 160], np.uint8), np.array([180, 25, 255], np.uint8))
 
+edges = cv2.Canny(edges, 100, 500)
 print "Edges detected: " + str(datetime.datetime.now())
 
 lines = cv2.HoughLinesP(edges, 1, np.pi/180, 75, minLineLength=250, maxLineGap=30)
@@ -25,26 +30,36 @@ lines = cv2.HoughLinesP(edges, 1, np.pi/180, 75, minLineLength=250, maxLineGap=3
 
 print "Lines detected: " + str(datetime.datetime.now())
 
-if lines is not None:
+if lines is None:
+    print "NO LINES DETECTED!"
+else:
+    theta = np.empty(len(lines))
+    d = np.empty(len(lines))
+    cnt = 0
     for line in lines:
         line = line.ravel()
+        theta[cnt] = (math.pi/2 - math.atan2(line[1]-line[3], line[2]-line[0])) % math.pi
+        d[cnt] = line[2]*math.cos(theta[cnt]) + line[3]*math.sin(theta[cnt])
+        print "Line detected: " + str(tuple(line[0:2])) + "; " + str(tuple(line[2:4])) + "\t->\tth=" + str(math.degrees(theta[cnt])) + ", d=" + str(d[cnt])
+        cnt += 1
         cv2.line(img, tuple(line[0:2]), tuple(line[2:4]), (0, 0, 255), 2)
-        continue
-        for rho, theta in line:
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a*rho
-            y0 = b*rho
-            x1 = int(x0 + 1000*(-b))
-            y1 = int(y0 + 1000*a)
-            x2 = int(x0 - 1000*(-b))
-            y2 = int(y0 - 1000*a)
 
-            cv2.line(img, (x1,y1), (x2,y2), (0,0,255), 2)
+        # cv2.imshow('Result', np.hstack((img, cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB))))
+        # plt.hist2d([math.degrees(t) for t in theta], d, bins=30, range=np.array([[0, 180], [0, 700]]))
+        # if cnt == 1: plt.colorbar()
+        # cv2.waitKey(1000)
+
+    plt.figure()
+    plt.hist2d([math.degrees((-t+math.pi/2) % math.pi) for t in theta], d, bins=30, range=np.array([[0, 180], [-600, 700]]))
+    plt.colorbar()
+
+    plt.figure()
+    plt.hist([math.degrees((-t+math.pi/2) % math.pi) for t in theta], bins=30, range=np.array([0, 180]))
+
 
 print "End: " + str(datetime.datetime.now())
 
 cv2.imshow('Result', np.hstack((img, cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB))))
-cv2.imwrite('findLine-lines-3.jpg', img)
-cv2.imwrite('findLine-edges-3.jpg', edges)
-cv2.waitKey(10000)
+# cv2.imwrite('findLine-lines-3.jpg', img)
+# cv2.imwrite('findLine-edges-3.jpg', edges)
+cv2.waitKey(0)
